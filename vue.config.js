c4b521aab28dcd1e68601d148ed36b5ae5c5295c
ViktoriyaStar <vika-star17@yandex.ru>
2020-05-02 11:15:22 +0300

    sxdc

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
-const webpack = require('webpack')
+
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jquery: 'jquery',
-                'window.jQuery': 'jquery',
-                jQuery: 'jquery'
-            })
-        ]
-    },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/FLClient/'
-    : '/'
-}
+    publicPath: "/FLClient",
+    outputDir: "docs"
+};
